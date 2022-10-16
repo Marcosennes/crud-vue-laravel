d5df19045590fd0da0b79acd2d669a8aea2ed9aa
Marcos <marcosvictoreb@gmail.com>
2022-10-16 02:27:00 -0300

    Métodos de detalhar e excluir cidade

--- backend/app/Http/Controllers/CidadeController.php
+++ backend/app/Http/Controllers/CidadeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CidadeRequest;
+use App\Http\Requests\CidadeCreateRequest;
 use App\Models\Cidade;
 use App\Models\Estado;
 use Illuminate\Http\Request;
@@ -19,7 +19,7 @@ class CidadeController extends Controller
         return response()->json(['cidades' => $cidades]);
     }
 
-    public function inserir(CidadeRequest $request){
+    public function inserir(CidadeCreateRequest $request){
         $cidade = Cidade::create($request->all());
         $cidade->estadoNome = Estado::find($cidade->estadoId)->nome;
         
@@ -28,5 +28,29 @@ class CidadeController extends Controller
             'message'   => 'Cidade inserida com sucesso.',
             'cidade'    => $cidade->getAttributes()
         ]);
+    }
+
+    public function detalhar($id){
+        $cidade = Cidade::find($id);
+        $cidade->estadoNome = Estado::find($cidade->estadoId)->nome;
+
+        return response()->json(['cidade' => $cidade]);
+    }
+
+    public function excluir($id){
+        $cidade = Cidade::find(intval($id));
+        $exclusao_confirmacao = $cidade->delete();
+
+        if($exclusao_confirmacao){
+            return response()->json([
+                'success'   => true,
+                'message'   => 'A cidade ' . $cidade->nome . ' foi excluída com sucesso.'
+            ]);
+        } else{
+            return response()->json([
+                'success'   => false,
+                'message'   => 'A cidade ' . $cidade->nome . ' não pode ser excluída.'
+            ]);
+        }
     }
 }
